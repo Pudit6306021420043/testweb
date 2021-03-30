b612ac3f5d00cef73ee7203b80c84b69dbe7cb12
Pudit6306021420043 <6306021420043@fitm.kmutnb.ac.th>
2021-03-30 13:47:26 +0700

    commit

--- newshopshock/product_list.php
+++ newshopshock/product_list.php
@@ -1,8 +1,7 @@
-<?php
-    session_status();
-    $_SESSION['cus_id']=1234;
+<?php 
+    session_start();
+    $_SESSION['cus_id']=1;
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,85 +10,68 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
-<body onload="loadDoc()">
-
-
-<center>
-<div id="out"></div><br>
-<div id="out2"></div>
-</center>
-
-
-
-<script>
-let arr;
-let label=["product_id","รหัสสินค้า","ชื่อสินค้า","brand","หน่วนนับ","ราคา","จำนวนสินค้า"];
-    function loadDoc(){
-        //alert("dddddd");
-        let out=document.getElementById("out");
-        xhttp = new XMLHttpRequest();//เปิดการสื่อสาร rest
+<body onload= "load_doc()">
+<center><div id="out"></div><br><div id="out2"></div><center>
+    <script>
+    let cus_id = 1;  
+    let arr;
+    let label = ["product_id","รหัสสินค้า","ชื่อสินค้า","brand","หน่วยนับ","ราคา","จำนวนสินค้า"];
+    function load_doc(){
+        let out = document.getElementById("out");
+        xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function(){
             if(this.readyState==4 && this.status==200){
-                alert(this.responseText);
                 arr = JSON.parse(this.responseText);
-                text="<table border='1'>";
-
+                alert(arr);
+                text = "<table border='1'>";
                 for(i=0;i<label.length-1;i++){
-                    text+="<th>"+label[i]+"</th>";
+                    text+= "<th>"+label[i]+"</th>";
                 }
-
-                text="<tr>"+text+"</tr>";
+                text = "<tr>"+text+"</tr>";
                 for(i=0;i<arr.length;i++){
                     for(j=0;j<arr[i].length-1;j++){
-                        text+="<td>"+arr[i][j]+"</td>";
+                        text += "<td>"+ arr[i][j]+"</td>";
                     }
-                    text+="<td>"+"<button onclick='sel_product("+i+")'>< ShopShock ></button>"+"</td>";
-                    text="<tr>"+text+"</tr>";
-                    
+                    text += "<td>"+"<button onclick='sel_product("+i+")'>< ShopShock ></button>"+"</td>";
+                    text = "<tr>"+text+"</tr>";
                 }
-                text+="</table>";
-                out.innerHTML=text;
+                text += "</table>";
+                out.innerHTML = text;
             }
         }
         xhttp.open("GET","product_rest.php",true);
         xhttp.send();
     }
 
-
     function sel_product(idx){
-        let out=document.getElementById("out2");
-        text="<form><table border='1'>";
-                for(i=0;i<label.length-1;i++){
-                    text+="<tr><th>"+label[i]+"</th>";
-                    text+="<td>"+arr[idx][i]+"</td></tr>";
-                }
-                text+="<tr><th>"+label[6]+"</th>";
-                text+="<td><input type='number' id='n_"+idx+"' min='1' max='"+arr[idx][6]+"'></td></tr>";
-                text+="<tr><td colspan=2><button inclick='open_po("+idx+")'>Add to cart </button><input type='reset'></td></tr>";
-                text+="</table>";
-                out.innerHTML=text;
+        let out = document.getElementById("out2");
+        text = "<table border='1'>";
+        for(i=0;i<label.length-1;i++){
+            text+= "<tr><th>"+label[i]+"</th>";
+            text+= "<td>"+arr[idx][i]+"</td></tr>";
+        }
+        text+= "<tr><th>"+label[6]+"</th>";
+        text+= "<td><input type='number' id='n_"+idx+"' min='1' max='"+arr[idx][6]+"' ></td></tr>"; 
+        text+= "<tr><td colspan=2><button onclick='open_po("+idx+")'>Add to Cart</button><input type='reset'></td></tr>";
+        text += "</table>";
+        out.innerHTML = text;
     }
-/*
+
     function open_po(idx){
-        let qty=document.getElementById("n_"+idx);
-        xhttp = new XMLHttpRequest();//เปิดการสื่อสาร rest
+        $p_qty = document.getElementById("n_"+idx);
+        xhttp = new XMLHttpRequest();
+        $p_id    =arr[idx][0]; //id
+        $p_price =arr[idx][5]; //price
         xhttp.onreadystatechange = function(){
             if(this.readyState==4 && this.status==200){
-                //alert(this.responseText);
-                arr = JSON.parse(this.responseText);
+                alert(this.responseText);
             }
         }
         xhttp.open("POST","product_rest.php",true);
         xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-        xhttp.send("id="+arrarr[idx][0]+"&code="+arrarr[idx][1]+"&qty="+qty.value);
+        xhttp.send("p_id="+$p_id+"&p_price="+$p_price+"&p_qty="+$p_qty.value);
     }
 
-*/
-
-
-    
-</script>
-    
+    </script>
 </body>
 </html>
--- newshopshock/product_rest.php
+++ newshopshock/product_rest.php
@@ -1,33 +1,53 @@
-<?php
-    session_status();
+<?php 
+    session_start();
 ?>
 <?php
     include_once("class.db.php");
     if($_SERVER["REQUEST_METHOD"]=='GET'){
         echo json_encode(product_list(),JSON_UNESCAPED_UNICODE);
+    }else if($_SERVER["REQUEST_METHOD"]=='POST'){
+        create_bill($_POST);
+        echo json_encode(print_r($_POST));
 
     }
-
     function product_list(){
-        $db=new database();
+        $db = new database();
         $db->connect();
-        $sql="SELECT Product_id,Product_code,Product_Name,
-                brand.Brand_name,unit.Unit_name,
-                product.Cost,product.Stock_Quantity
-            FROM product,brand,unit
-            WHERE product.Brand_ID = brand.Brand_id
-            AND product.Unit_ID=unit.Unit_id";
+        $sql = "SELECT Product_id,Product_code,Product_Name,
+                       brand.Brand_name, unit.Unit_name,
+                       product.Cost, product.Stock_Quantity
+                FROM  product,brand,unit 
+                WHERE product.Brand_ID = brand.Brand_id
+                and   product.Unit_ID  = unit.Unit_id";
         $result = $db->query($sql);
         $db->close();
         return $result;
     }
-
-    /*function create_bill($_POST){
-        $db=new database();
+    function create_bill($post){
+        $db = new database();
         $db->connect();
-        $sql="SELECT Bill_id,Cus_ID";
-        $result = $db->query($sql);
+        // 1check first bill?
+        // 1.1 yes bill_id=1 , bill sta = 0;
+        //         add bill+detail - > bill_id, product_id , qty, price
+        // 1.2 chack last bill_stat of user?
+        //     check p_id is exist
+        //     add
+        // bill_id = last_id
+        $result = $db->exec("SELECT Bill_id, Cus_ID, Bill_Status FROM bill WHERE Cus_ID=1 and Bill_Status=0");
+        if($result->num_rows==0){
+            $result = $db->query("SELECT Bill_id FROM bill order by Bill_id DESC limit 1");
+            echo sizeof($result);
+            if(sizeof($result)==0){
+                $db->exec("INSERT INTO bill(Bill_id, Cus_ID,Bill_Status) VALUES (1,1,0)");
+                $result = $db->query("SELECT Bill_id FROM bill WHERE cus_ID=1 order by Bill_id DESC limit 1");
+            }else{
+            
+            }
+            $result = $db->query("SELECT Bill_id FROM bill WHERE cus_ID=1 order by Bill_id DESC limit 1");
+            $db->exec("INSERT INTO bill_detail(Bill_id, Product_ID, Quantity, Unit_Price) 
+                       VALUES {$result[0]},{$post['pid']},{$post['qty']},{$post['price']})");
+        }
         $db->close();
         return $result;
-    }*/
+    }
 ?>
